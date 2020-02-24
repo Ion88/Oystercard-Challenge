bde4c1b7210770ff66640e04cf57e62e217978c8
Ion <ion.vizitiu88@gmail.com>
2020-02-24 16:03:53 +0000

    chapter 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,11 +5,28 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false 
   end
 
   def top_up(money)
-    fail "Maximum balance of #{LIMIT} exceeded" if @balance + money > LIMIT 
+    fail "Maximum balance of #{LIMIT} exceeded" if @balance + money > LIMIT
     @balance += money
+  end
+
+  def deduct(money)
+    @balance -= money
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,5 +23,38 @@ describe Oystercard do
   end
  end
 
+ describe '#deduct' do
+   it 'can deduct money from card' do
+     expect(subject).to respond_to(:deduct).with(1).argument
+   end
+
+   it 'can take money from the card' do
+     subject.deduct(5)
+     expect(subject.balance).to eq -5
+   end
+ end
+
+ describe '#touch_in' do
+   it 'can touch in' do
+     expect(subject).to respond_to(:touch_in)
+   end
+
+   it 'knows when you are in journey' do
+     subject.touch_in
+     expect(subject.in_journey?).to eq true
+   end
+ end
+
+ describe '#touch_out' do
+   it 'can touch out' do
+     expect(subject).to respond_to(:touch_out)
+   end
+
+   it 'knows when you are in journey' do
+     subject.touch_out
+     expect(subject.in_journey?).to eq false 
+   end
+ end
+
 
 end
